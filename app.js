0ec4327925b3a5f7d1b2d01fb203afd96ab71116
drein100 <atakankaratas@Atakan-MacBook-Air.local>
2024-11-25 11:33:11 +0300

    updates

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const series = ["1", "A", "2", "B", "3", "C", "4", "Ç", "5", "D", "6", "E", "7", "F", "8", "G", "9", "Ğ", "10", "H", "11", "I", "12", "İ", "13", "J"];
+const series = ["1", "A", "2", "B", "3", "C", "4", "Ç", "5", "D", "6", "E", "7", "F", "8", "G", "9", "Ğ", "10", "H", "11", "I", "12", "İ", "13"];
 const display = document.getElementById("display");
-let speed = 100;
+let speed = 12;
 let interval;
 let index = 0;
 let cycleCount = 0;
@@ -11,21 +11,8 @@ function displayOneByOne() {
       if (index < series.length) {
         display.textContent = series[index];
         index++;
-      } else {
-        index = 0;
-        cycleCount++;
-        if (cycleCount >= 2) {
-          clearInterval(interval);
-        }
-      }
+      } 
     }, speed);
   }
 
 displayOneByOne();
-
-document.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    speed = 1000; 
-    displayOneByOne();
-  }
-});
