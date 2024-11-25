a491eb4d3a0cb89fb9f6426890bd32ea69643734
drein100 <atakankaratas@Atakan-MacBook-Air.local>
2024-11-25 11:42:44 +0300

    updates

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const series = ["1", "A", "2", "B", "3", "C", "4", "Ç", "5", "D", "6", "E", "7", "F", "8", "G", "9", "Ğ", "10", "H", "11", "I", "12", "İ", "13"];
+const series = ["1", "A", "2", "B", "3", "C", "4", "Ç", "5", "D", "6", "E", "7", "F", "8", "G", "9", "Ğ", "10", "H", "11", "I", "12", "İ", "13" , ""];
 const display = document.getElementById("display");
 let speed = 12;
 let interval;
